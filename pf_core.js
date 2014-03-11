e40f55acbcc0e6b7a46e00596a7a48e77496113a
FTWinston <ftwinston@gmail.com>
2014-03-11 00:23:10 +0000

    Added all racial attributes (as comments only, nothing is implemented)

--- pf_core.js
+++ pf_core.js
@@ -71,19 +71,135 @@ dwarf.extend({
 	adjustSkills: function() {
 		console.log("dwarf skills");
 	}
+/*
+	base speed 20, never modified by armor or encumbrance
+	Darkvision 60 ft
+	+4 dodge to AC vs giants
+	+2 racial to Appraise skill for nonmagic goods containing precious metal or gemstones
+	+1 bonus on attack rolls vs orc & goblinoid humanoids
+	+2 racial bonus on saving throws against poison, spells & SLA
+	+4 racial bonus to CMD when resisting bull rush or trip, if standing on the ground
+	+2 bonus to perception to notice unusual stonework. Check whenever within 10 feet of such features, even when not looking
+	
+	proficient with battleaxes, heavy picks, warhammers
+	any weapon with "dwarven" in its name is martial
+	
+	initial languages: Common & Dwarven
+	language selection: Giant, Gnome, Goblin, Orc, Terran & Undercommon
+*/
 });
 
 var elf = new Race("Elf", Size_Medium,			30, [0, 2, -2, 2, 0, 0]);
-
+elf.extend({
+/*
+	low light vision: can see twice as far as humans in dim light
+	immune to magic sleep effects
+	+2 racial saving throw bonus against enchantment spells & effects
+	+2 racial bonus on caster level checks made to overcome spell resistance
+	+2 racial bonus on spellcraft checks to identify properties of magic items
+	+2 to perception checks
+	
+	proficient with longbows (including composite longbows), longswords, rapiers, and shortbows (including composite shortbows)
+	any weapon with "elven" in its name is martial
+	
+	initial languages: Common & Elven
+	language selection: Celestial, Draconic, Gnoll, Gnome, Goblin, Orc & Sylvan
+*/
+});
 var gnome = new Race("Gnome", Size_Small,		20, [-2, 0, 2, 0, 0, 2]);
+gnome.extend({
+/*
+	small size - +1 to AC, +1 to attack rolls, -1 to CMB and CMD, +4 size bonus to stealth checks
+	
+	Low light vision
+	
+	+4 dodge bonus to AC vs giants
+	+1 to the DC of any saving throws against illusion spells they cast.
+	
+	If charisma > 11, gain the following SLAs 1/day:
+	dancing lights, ghost sound, prestigitation and speak with animals.
+	For each, caster level is gnome's level.
+	DC is 10 + spell's level + charisma modifier.
+	
+	+1 on attack roles against reptilian and goblinoid humanoids.
+	+2 racial saving throw against illusion spells & effects
+	+2 to perception checks
+	+2 racial bonus on a craft or profession skill of their choice (Obssesive)
+	
+	any weapon with "gnome" in its name is martial
+	
+	initial languages: Common, Gnome & Sylvan
+	language selection: Draconic, Dwarven, Elven, Giant, Goblin, & Orc.
+*/
+});
 
 var halfElf = new Race("Half-Elf", Size_Medium,	30, null);
+halfElf.extend({
+/*
+	Low light vision
+	
+	Skill Focus as a bonus feat at 1st level.
+	
+	immune to magic sleep effects
+	+2 racial saving throw bonus against enchantment spells & effects
+	+2 to perception checks
+	
+	Choose two favored classes at first level instead of one.
+	
+	initial languages: Common & Elven
+	language selection: any (but not secret languages, such as druidic)
+*/
+});
 
 var halfOrc = new Race("Half-Orc", Size_Medium,	30, null);
+halfOrc.extend({
+/*
+	Darkvision 60 ft
+	
+	+2 to intimidate checks
+	
+	Once per day, when brought below 0 hp but not killed, can fight on for 1 more round as if disabled.
+	
+	proficient with greataxes and falchions
+	any weapon with "orc" in its name is martial
+	
+	initial languages: Common & Orc
+	language selection:  Abyssal, Draconic, Giant, Gnoll, & Goblin.
+*/
+});
 
 var halfling = new Race("Halfling", Size_Small,	20, [-2, 2, 0, 0, 0, 2]);
+halfling.extend({
+/*
+	small size - +1 to AC, +1 to attack rolls, -1 to CMB and CMD, +4 size bonus to stealth checks
+	
+	+1 on all saving throws
+	+2 on all saving throws against fear (in addition)
+	+2 to perception checks
+	+2 to acrobatics and climb checks
+	
+	proficient with slings
+	any weapon with "halfling" in its name is martial
+	
+	initial languages: Common & Halfling
+	language selection: Dwarven, Elven, Gnome, & Goblin
+*/
+});
 
 var human = new Race("Human", Size_Medium,		30, null);
+human.extend({
+/*
+	Select 1 bonus feat at 1st level
+	
+	Gain an additional skill at every level.
+	
+	proficient with 
+	any weapon with "" in its name is martial
+	
+	initial languages: Common
+	language selection: any (but not secret languages, such as druidic)
+*/
+});
 
 Races.push(dwarf, elf, gnome, halfElf, halfOrc, halfling, human);
 
